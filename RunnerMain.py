91ed364ac7c727f7ab73c6042f268a9d9d8593bf
AmitkiranPSPL <amitkiran.das@primawavesoft.com>
2026-01-14 15:34:45 +0530

    Update RunnerMain.py

--- RunnerMain.py
+++ RunnerMain.py
@@ -3,7 +3,7 @@ from datetime import datetime
 def main():
     with open("abcd.txt", "w", encoding="utf-8") as f:
         f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
-    print("AGF")
+    print("Hello World")
     return 54
 
 if __name__ == "__main__":
